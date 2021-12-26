dbbf8fb672128f9c64ddfbbfc8d105b5fbdaa849
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-26 22:22:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import numpy as np
 
 if __name__ == __main__:
 
+image = cv2.imread('CX9.jpg')
 
-
-cv2.nameWindow("CX9",cv2.WINDOW_NORMAL)
-cv2.imshow("CX9",img)
+cv2.nameWindow('CX9',cv2.WINDOW_NORMAL)
+cv2.imshow('CX9',img)
 cv2.waitKey(0)
 cv2.destroyALLWindows()
