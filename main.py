95aee994ddf172e52ff776ba7e69a8ad0ab4c9df
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2022-01-06 20:13:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import numpy as np
-
-def main():
-    st.set_page_config(layout="wide")
-    menu = ['🐱', '🐶'] 
-    st.sidebar.header('P1')
-    st.sidebar.selectbox('選擇',menu)
-    
+   
 # From: https://stackoverflow.com/questions/39922967/python-determine-tic-tac-toe-winner
 def checkRows(board): #檢查行
     for row in board:
